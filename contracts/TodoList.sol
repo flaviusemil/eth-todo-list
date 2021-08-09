bbbe2af90804cb13da0267db8cd146d01380a720
Flavius Condurache <flavius.condurache@gmail.com>
2021-08-10 00:13:54 +0300

    Add task complete toggle functionality

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -18,6 +18,11 @@ contract TodoList {
         bool completed
     );
 
+    event TaskUpdated(
+        uint id,
+        bool completed
+    );
+
     constructor() public {
         createTask("Check if it's working...");
     }
@@ -26,5 +31,12 @@ contract TodoList {
         tasks[taskCount] = Task(taskCount, _content, false);
         taskCount++;
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskUpdated(_id, _task.completed);
     }
 }
